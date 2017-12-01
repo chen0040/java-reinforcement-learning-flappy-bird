0f5bfe130faa68413f88639f5aa6a718a8651fbf
xschen <xs0040@gmail.com>
2017-12-01 22:38:47 +0800

    map state

--- src/main/java/com/github/chen0040/jrl/flappybird/bots/Bot.java
+++ src/main/java/com/github/chen0040/jrl/flappybird/bots/Bot.java
@@ -2,7 +2,7 @@ package com.github.chen0040.jrl.flappybird.bots;
 
 public class Bot {
 
-    public static String mapState(double xdif, double ydif, int vel) {
+    public static int mapState(double xdif, double ydif, int vel) {
 
         // Map the (xdif, ydif, vel)to the respective state, with regards to the grids
         // The state is a string, "xdif_ydif_vel"
@@ -21,6 +21,24 @@ public class Bot {
             ydif = (int)(ydif) - ((int)(ydif) % 60);
         }
 
-        return ((int)(xdif)) + "_" + ((int)(ydif)) + "_" + vel;
+        return ((int)(xdif)) * 1000000 + ((int)(ydif)) * 1000 + vel;
+    }
+
+    public int act(double xdif, double ydif, int vel){
+
+    //Chooses the best action with respect to the current state - Chooses 0 (don't flap) to tie-break
+
+    String state = self.map_state(xdif, ydif, vel)
+
+            self.moves.append( [self.last_state, self.last_action, state] ) # Add the experience to the history
+
+    self.last_state = state # Update the last_state with the current state
+
+        if self.qvalues[state][0] >= self.qvalues[state][1]:
+    self.last_action = 0
+            return 0
+            else:
+    self.last_action = 1
+            return 1
     }
 }
